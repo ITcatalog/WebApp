77e2897620cd13bd35e2ddee679dda997681d004
Jonas <jetschni@gmail.com>
2016-02-16 14:36:04 +0100

    Reorganised and cleanup of directory

---
+++ src/content/category.php
@@ -0,0 +1,40 @@
+<?php
+
+include ('template/categoryCard.php');
+
+if(isset($_GET['cat'])){
+
+	$category = urldecode($_GET['cat']);
+	$sparql = '
+
+	SELECT ?service ?prefLabel ?abstract ?bgColor
+	{
+		?service (itcat:inCategory | schema:provider) <'.$category.'>;
+	  skos:prefLabel ?prefLabelLang;
+		dcterms:abstract ?abstractLang.
+		OPTIONAL{
+			GRAPH ?g {
+				<'.$category.'> itcat_app:hasBgColor ?bgColor
+			}
+		}
+	  FILTER (langMatches(lang(?prefLabelLang),"'.LANG.'"))
+		FILTER (langMatches(lang(?abstractLang),"'.LANG.'"))
+		BIND (str(?prefLabelLang) AS ?prefLabel)
+		BIND (str(?abstractLang) AS ?abstract)
+	}
+	ORDER BY ?prefLabel
+	';
+
+	$result = $db->query( $sparql );
+	if( !$result ) { print $db->errno() . ": " . $db->error(). "\n"; exit; }
+
+	$colorValue = 300;
+	while( $row = $result->fetch_array() ){
+
+		if(!isset($row['bgColor'])){
+	    $row['bgColor'] = 'grey';
+			$colorValue = '';
+	  }
+		showCardTemplate ($row['service'], $row['prefLabel'], $row['abstract'], '', $row['bgColor'], '?c=service&service=', 4, $colorValue);
+	}
+}
